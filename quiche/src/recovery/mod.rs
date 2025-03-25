edc38536a830a0cca26e340c580749f8624fe562
Antonio Vicente <avicente@cloudflare.com>
2025-03-25 01:07:58 -0700

    add RecoveryApi trait to allow selecting between the legacy and gcongestion implementations

--- quiche/src/recovery/mod.rs
+++ quiche/src/recovery/mod.rs
@@ -29,6 +29,8 @@ use std::time::Duration;
 use std::time::Instant;
 
 use crate::frame;
+use crate::packet;
+use crate::ranges::RangeSet;
 use crate::Config;
 
 #[cfg(feature = "qlog")]
@@ -39,7 +41,7 @@ use smallvec::SmallVec;
 mod congestion;
 mod rtt;
 
-pub use self::congestion::recovery::Recovery;
+use self::congestion::recovery::LegacyRecovery;
 
 // Loss Recovery
 const INITIAL_PACKET_THRESHOLD: u64 = 3;
@@ -114,6 +116,98 @@ impl RecoveryConfig {
             initial_congestion_window_packets: config
                 .initial_congestion_window_packets,
         }
+    }
+}
+
+#[enum_dispatch::enum_dispatch(RecoveryApi)]
+#[allow(clippy::large_enum_variant)]
+#[derive(Debug)]
+pub(crate) enum Recovery {
+    Legacy(LegacyRecovery),
+}
+
+#[enum_dispatch::enum_dispatch]
+/// Api for the Recovery implementation
+pub trait RecoveryApi {
+    fn lost_count(&self) -> usize;
+    fn bytes_lost(&self) -> u64;
+
+    /// Returns whether or not we should elicit an ACK even if we wouldn't
+    /// otherwise have constructed an ACK eliciting packet.
+    fn should_elicit_ack(&self, epoch: packet::Epoch) -> bool;
+
+    fn get_acked_frames(&mut self, epoch: packet::Epoch) -> Vec<frame::Frame>;
+
+    fn get_lost_frames(&mut self, epoch: packet::Epoch) -> Vec<frame::Frame>;
+
+    fn get_largest_acked_on_epoch(&self, epoch: packet::Epoch) -> Option<u64>;
+    fn has_lost_frames(&self, epoch: packet::Epoch) -> bool;
+    fn loss_probes(&self, epoch: packet::Epoch) -> usize;
+    #[cfg(test)]
+    fn inc_loss_probes(&mut self, epoch: packet::Epoch);
+
+    fn ping_sent(&mut self, epoch: packet::Epoch);
+
+    fn on_packet_sent(
+        &mut self, pkt: Sent, epoch: packet::Epoch,
+        handshake_status: HandshakeStatus, now: Instant, trace_id: &str,
+    );
+    fn get_packet_send_time(&self) -> Instant;
+
+    fn on_ack_received(
+        &mut self, ranges: &RangeSet, ack_delay: u64, epoch: packet::Epoch,
+        handshake_status: HandshakeStatus, now: Instant, trace_id: &str,
+    ) -> (usize, usize, usize);
+
+    fn on_loss_detection_timeout(
+        &mut self, handshake_status: HandshakeStatus, now: Instant,
+        trace_id: &str,
+    ) -> (usize, usize);
+    fn on_pkt_num_space_discarded(
+        &mut self, epoch: packet::Epoch, handshake_status: HandshakeStatus,
+        now: Instant,
+    );
+    fn on_path_change(
+        &mut self, epoch: packet::Epoch, now: Instant, _trace_id: &str,
+    ) -> (usize, usize);
+    fn loss_detection_timer(&self) -> Option<Instant>;
+    fn cwnd(&self) -> usize;
+    fn cwnd_available(&self) -> usize;
+    fn rtt(&self) -> Duration;
+
+    fn min_rtt(&self) -> Option<Duration>;
+
+    fn rttvar(&self) -> Duration;
+
+    fn pto(&self) -> Duration;
+
+    fn delivery_rate(&self) -> u64;
+
+    fn max_datagram_size(&self) -> usize;
+
+    fn pmtud_update_max_datagram_size(&mut self, new_max_datagram_size: usize);
+
+    fn update_max_datagram_size(&mut self, new_max_datagram_size: usize);
+
+    fn on_app_limited(&mut self);
+
+    #[cfg(test)]
+    fn app_limited(&self) -> bool;
+
+    fn update_app_limited(&mut self, v: bool);
+
+    fn delivery_rate_update_app_limited(&mut self, v: bool);
+
+    fn update_max_ack_delay(&mut self, max_ack_delay: Duration);
+
+    #[cfg(feature = "qlog")]
+    fn maybe_qlog(&mut self) -> Option<EventData>;
+    fn send_quantum(&self) -> usize;
+}
+
+impl Recovery {
+    pub fn new_with_config(recovery_config: &RecoveryConfig) -> Self {
+        Recovery::Legacy(LegacyRecovery::new_with_config(recovery_config))
     }
 }
 
